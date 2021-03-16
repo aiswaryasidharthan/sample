97f1d95c725786a3293e18194800c935621cbddb
AISWARYA SIDHARTHAN <aiswaryasidharth@gmail.com>
2021-03-16 17:49:42 +0000

    Trial

---
+++ Shop_trial/Models/CategoryRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop_trial.Models
+{
+    public class CategoryRepository
+    {
+    }
+}
---
+++ Shop_trial/Models/Shop.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop_trial.Models
+{
+    public class Shop
+    {
+        public int ShopId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; }
+        public string ImageThumbnailUrl { get; set; }
+        public bool IsOnSale { get; set; }
+        public bool OnStock { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
